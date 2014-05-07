031accc4165a624e884ee4fcfea8164037842598
Cinthia Davalos <cinthia@mijobrands.com>
2014-05-07 09:54:30 -0500

    added test Class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Cinthia Davalos on 5/7/14.
+//  Copyright (c) 2014 Miguel Carrillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Cinthia Davalos on 5/7/14.
+//  Copyright (c) 2014 Miguel Carrillo. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
